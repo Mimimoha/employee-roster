938202e5cd6c58d965eb0c37ae8d4b802a030825
Mimimoha <mohamedm1987@gmail.com>
2022-11-07 11:04:10 -0600

    changed employer test file veriables

--- test/employee.test.js
+++ test/employee.test.js
@@ -5,45 +5,45 @@ describe('employee', () => {
     describe('Initialization', () => {
         // it how to pass the test 
         it(" should create a card of employee information when ", () => {
-            const instance = new Employee()
-            expect(typeof (instance)).toBe("object")
+            const newEmployee = new Employee()
+            expect(typeof (newEmployee)).toBe("object")
         })
         it(" should be able to take in a string for name", () => {
             const name = "mariam"
 
-            const instance = new Employee(name)
-            expect(instance.name).toBe(name)
+            const newEmployee = new Employee(name)
+            expect(newEmployee.name).toBe(name)
         })
         it("get name function should be able to get name", () => {
             const name = "mariam"
 
-            const instance = new Employee(name)
-            expect(instance.getname()).toBe(name)
+            const newEmployee = new Employee(name)
+            expect(newEmployee.getname()).toBe(name)
         })
         it(" should be able to take in a number for id", () => {
             const id = 0
 
-            const instance = new Employee('test', id)
-            expect(instance.id).toBe(id)
+            const newEmployee = new Employee('test', id)
+            expect(newEmployee.id).toBe(id)
         })
         it("get id function should be able to get id", () => {
             const id = 0
 
-            const instance = new Employee("test", id)
-            expect(instance.getid()).toBe(id)
+            const newEmployee = new Employee("test", id)
+            expect(newEmployee.getid()).toBe(id)
         })
         it('should be able to take in a string for email', () => {
             const email = "email"
 
-            const instance = new Employee('name', 0, 'email')
-            expect(instance.email).toBe(email)
+            const newEmployee = new Employee('name', 0, 'email')
+            expect(newEmployee.email).toBe(email)
 
         })
         it("get name function should be able to get name", () => {
             const email = "email"
 
-            const instance = new Employee('name', 0, email)
-            expect(instance.getemail()).toBe(email)
+            const newEmployee = new Employee('name', 0, email)
+            expect(newEmployee.getemail()).toBe(email)
 
         })
 
